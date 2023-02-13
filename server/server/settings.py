922cd5658ed806ed9dc59f0447bf067d9d2322a4
Chris <christopherduggan@gmail.com>
2023-02-13 11:54:43 -0500

    edit for psql

--- server/server/settings.py
+++ server/server/settings.py
@@ -120,8 +120,11 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'jwt_auth_lesson',
+        'USER': 'jwt_auth_lesson_admin',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
     }
 }
 
